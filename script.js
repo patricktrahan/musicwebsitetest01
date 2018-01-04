ffcd7dbc26c0f4913c0c361542a4355d2c8cf4e2
Patrick Trahan <34581132+patricktrahan@users.noreply.github.com>
2018-01-04 00:25:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* open sidenav */
+function openNav() {
+	document.getElementById('mysidenav').style.width = '40%';
+}
+
+/* close sidenav */
+function closeNav() {
+	document.getElementById('mysidenav').style.width = '0%';
+}
+
